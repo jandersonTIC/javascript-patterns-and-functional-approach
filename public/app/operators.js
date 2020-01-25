41951b19b20f869567269a5a8d999f5aaea69234
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-25 20:27:55 -0300

    add Unix/Linux Pipe operator at operators module

--- public/app/operators.js
+++ public/app/operators.js
@@ -1,8 +1,15 @@
 export const partialize = (fn, ...params) =>
     fn.bind(null, ...params);
 
+// Math Composition
 export const compose = (...fns) => value =>
     fns.reduceRight(
+        (previousValue, fn) => fn(previousValue),
+        value
+    );
+// Unix/Linux Pipe operator
+export const pipe = (...fns) => value =>
+    fns.reduce(
         (previousValue, fn) => fn(previousValue),
         value
     );
