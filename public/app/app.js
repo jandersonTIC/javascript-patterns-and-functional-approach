9715eee58d38305c97a45997be79b3ac04c0f4a6
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-25 20:51:50 -0300

    use takeUntil function to limite max 3 calls on Notas service

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,11 @@
 import "./arrayHelpers.js";
 import { notaService as service } from "./nota/service.js";
 import { log } from "./handles.js";
+import { takeUntil } from "./operators.js";
 
 document
     .querySelector("#myButton")
-    .onclick = () => service
+    .onclick = takeUntil(3, () => service
         .sumItemsFromNotasWhereCodeIsEqualTo("2143")
         .then(log)
-        .catch(log);
+        .catch(log));
