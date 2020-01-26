02abd8f881b15cb2ba2a15f3f042066a88a5c525
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-25 22:17:25 -0300

    add timeoutPromise function at handles file

--- public/app/handles.js
+++ public/app/handles.js
@@ -5,3 +5,15 @@ export const log = param => {
     console.log(param);
     return param;
 };
+
+export const timeoutPromise = (milliseconds, promise) => {
+    const timeout = new Promise((resolve, reject) =>
+        setTimeout(() =>
+            reject(`Limite da promisse excedido (limite: ${milliseconds} ms)`),
+            milliseconds)
+        );
+    return Promise.race([
+        timeout,
+        promise
+    ])
+};
