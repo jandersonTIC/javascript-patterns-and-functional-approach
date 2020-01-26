2b46c1f4bcfff8bc76e780580ddde9c6e569fba2
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-25 21:11:51 -0300

    use debounceTime on click event for protect Notas service API

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,14 @@
 import "./arrayHelpers.js";
 import { notaService as service } from "./nota/service.js";
 import { log } from "./handles.js";
-import { takeUntil } from "./operators.js";
+import { takeUntil, debounceTime } from "./operators.js";
 
 document
     .querySelector("#myButton")
-    .onclick = takeUntil(3, () => service
-        .sumItemsFromNotasWhereCodeIsEqualTo("2143")
-        .then(log)
-        .catch(log));
+    .onclick = debounceTime(500,
+        takeUntil(3, () => service
+            .sumItemsFromNotasWhereCodeIsEqualTo("2143")
+            .then(log)
+            .catch(log)
+        )
+    );
