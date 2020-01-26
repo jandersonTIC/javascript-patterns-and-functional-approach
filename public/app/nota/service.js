4c7069f5c3ecd78a3776e00b1530191923840e9a
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-26 19:57:54 -0300

    use Maybe Monad at Notas service

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,42 +1,40 @@
 import { handleStatus } from '../handles.js';
 import { partialize, compose } from '../operators.js';
+import { Maybe } from "../monad/maybe.js";
 
 const API = "http://localhost:3000/notas";
 
-export const getItemsFromNotas = notas =>
-    notas.$flatMap(nota => nota.itens);
+export const getItemsFromNotas = notasMonad =>
+    notasMonad.map(notas => notas.$flatMap(nota => nota.itens));
 
-export const filterItemsByCode = (code, items) =>
-    items.filter(item => item.codigo === code);
+export const filterItemsByCode = (code, itemsMonad) =>
+    itemsMonad.map(items => items.filter(item => item.codigo === code));
 
-export const sumItemsValue = items =>
-    items.reduce((total, item) => total + item.valor, 0);
-
-export const sumItemsFromNotasWhereCodeIsEqualTo = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0);
+export const sumItemsValue = itemsMonad =>
+    itemsMonad.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notaService = {
     listAll(){
         return fetch(API)
             .then(handleStatus)
+            .then(notas => Maybe.of(notas))
             .catch(error => {
                 console.error(error);
                 return Promise.reject("Não foi possível obter as Notas!");
             });
     },
     sumItemsFromNotasWhereCodeIsEqualTo(code){
-        // Using Partial Application (fn partialize) and
+        // Partial Application (fn partialize)
+        const filterItems = partialize(filterItemsByCode, code);
         // Composition with Point-free Style.
+        const sumItems = compose(
+            sumItemsValue,
+            filterItems,
+            getItemsFromNotas
+        );
         return this
             .listAll()
-            .then(
-                compose(
-                    sumItemsValue,
-                    partialize(filterItemsByCode, code),
-                    getItemsFromNotas
-                )
-            );
+            .then(sumItems)
+            .then(result => result.getOrElse(0));
     }
 }
