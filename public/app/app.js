f059ee3ac7708737dd24f88ce3275eb49d1b79ef
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-25 21:38:36 -0300

    compose takeUntil and debounceTime functions at on click event

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,21 @@
 import "./arrayHelpers.js";
 import { notaService as service } from "./nota/service.js";
 import { log } from "./handles.js";
-import { takeUntil, debounceTime } from "./operators.js";
+import {
+    takeUntil,
+    debounceTime,
+    partialize,
+    compose
+} from "./operators.js";
 
-document
-    .querySelector("#myButton")
-    .onclick = debounceTime(500,
-        takeUntil(3, () => service
-            .sumItemsFromNotasWhereCodeIsEqualTo("2143")
-            .then(log)
-            .catch(log)
-        )
-    );
+const getNotas = () => service
+    .sumItemsFromNotasWhereCodeIsEqualTo("2143")
+    .then(log)
+    .catch(log);
+
+const action = compose(
+    partialize(debounceTime, 500),
+    partialize(takeUntil, 3)
+)(getNotas);
+
+document.querySelector("#myButton").onclick = action;
