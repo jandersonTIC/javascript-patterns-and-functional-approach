a5c2523de6e644e988bebd51f399722e6f9ef256
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-27 01:25:48 -0300

    use decorate logExecutionTime to Person methods

--- public/app/app2.js
+++ public/app/app2.js
@@ -1,4 +1,11 @@
 import { Person } from "./models/Person.js";
+import { decorate } from "./decorate.js";
+import { logExecutionTime } from "./models/decorators.js";
+
+decorate(Person, {
+    speak: logExecutionTime,
+    getFullName: logExecutionTime
+})
 
 const person =  new Person("Janderson", "Silva");
 
