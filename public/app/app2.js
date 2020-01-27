504fb6f2d72bdfac84749cc8fc93005fd56b7988
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-27 02:04:24 -0300

    use decorator after class declaration

--- public/app/app2.js
+++ public/app/app2.js
@@ -1,11 +1,4 @@
 import { Person } from "./models/Person.js";
-import { decorate } from "./decorate.js";
-import { logExecutionTime, inspectMethod } from "./models/decorators.js";
-
-decorate(Person, {
-    speak: [inspectMethod({ excludeReturn: true }), logExecutionTime],
-    getFullName: [logExecutionTime]
-})
 
 const person =  new Person("Janderson", "Silva");
 
--- public/app/models/Person.js
+++ public/app/models/Person.js
@@ -1,3 +1,6 @@
+import { inspectMethod, logExecutionTime } from "./decorators.js";
+import { decorate } from "../decorate.js";
+
 export class Person {
     constructor(name, surname) {
         this._name = name;
@@ -5,12 +8,18 @@ export class Person {
     }
 
     speak(phrase) {
-        const result = `${this._name} is speaking: ${phrase}`;
-        return result;
+        return `${this._name} is speaking: ${phrase}`;
     }
 
     getFullName() {
-        const result = `${this._name} ${this._surname}`;
-        return result;
+        return `${this._name} ${this._surname}`;
     }
 }
+
+decorate(Person, {
+    speak: [
+        inspectMethod({ excludeReturn: true }),
+        logExecutionTime
+    ],
+    getFullName: [logExecutionTime]
+})
