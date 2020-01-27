6848c0c1d3c70ff28de0bfedc10c60aaafd1aa65
Janderson Silva <janderson.silva@mandic.net.br>
2020-01-27 01:23:31 -0300

    add logExecutionTime decorator

---
+++ public/app/models/decorators.js
@@ -0,0 +1,8 @@
+export const logExecutionTime = (method, property, args) => {
+    console.log(`Método decorado: ${property}`);
+    console.log(`Argumentos do método: ${args}`);
+    console.time(property);
+    const result = method(...args);
+    console.timeEnd(property);
+    console.log(`Resultado do método: ${result}`);
+}
